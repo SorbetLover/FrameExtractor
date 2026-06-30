87c1a01332bd5947b9bed46496b487b4124c0f91
SorbetLover <umc4r4aa3@gmail.com>
2026-06-30 18:45:17 -0300

    download from yt via yt-dlp

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
 const path = require("path");
 const { exec, spawn } = require("child_process");
 const fs = require("fs");
 const { versions } = require("process");
+const { platform } = require("os");
+const { warn } = require("console");
+// const open = require('open');    
 
 function createWindow() {
-    const win = new BrowserWindow({
+    const wind = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
@@ -15,12 +18,33 @@ function createWindow() {
         icon: path.join(__dirname, "maomao.png"),
     });
 
-    win.loadFile("index.html");
-
-    fs.mkdirSync("./output");
-    fs.mkdirSync("./input");
+    wind.loadFile("index.html");
+    
+    if(!fs.existsSync("./output")) fs.mkdirSync("./output/");
+    if(!fs.existsSync("./input")) fs.mkdirSync("./input/");
+    // if(fs.existsSync("yt-dlp"))
+    // let ytpresent = false;
+    // let ffpresent = false;
+    // if(process.platform == "win32"){
+    //     for(let file of fs.readdirSync("./")){
+    //         // if(!file.startsWith("yt-dlp")) continue;
+    //         if(file.startsWith("ffmpeg")) ffpresent = true;
+    //         if(file.startsWith("yt-dlp")) ytpresent = true;
+    //     }
+    //     console.log((ytpresent ? "" : "Install yt-dlp binary" + ((process.platform == "win32") ? "in the root folder" : "in the system")))
+    //     console.log((ffpresent ? "" : "Install ffmpeg binary " + ((process.platform == "win32") ? "in the root folder" : "in the system")))
+    //     // if(!ffpresent) openLink("https://www.ffmpeg.org/download.html");
+    //     // if(!ytpresent) openLink("https://github.com/yt-dlp/yt-dlp/releases/");
+    //     // alert("test")
+    //     if(!ytpresent || !ffpresent) dialog.showMessageBox(wind, {
+    //         type: 'info',
+    //         title: 'Missing dependencies',
+    //         message: "You must install " + (!ffpresent ? "FFMpeg" : "") + (!ffpresent && !ytpresent ? " and " : "") + (!ytpresent ? "yt-dlp" : ""),
+    //         buttons: ['OK']
+    //     })
+    // }
 }
-
+// app.whenReady().
 app.whenReady().then(createWindow);
 
 ipcMain.on("extract", (event, cmddd) => {
@@ -44,7 +68,10 @@ ipcMain.on("makeFolder", (event, arg1) => {
     fs.mkdirSync("./output/" + arg1 + "/output/", { recursive: true });
     fs.mkdirSync("./output/" + arg1 + "/keep/", { recursive: true });
     fs.mkdirSync("./output/" + arg1 + "/output/video/", { recursive: true });
+    // console.log("AAA");
+
 });
+
 
 ipcMain.on(
     "extractframes", (event, videoe, frome, toe, olde, usepng, cutvid) => {
@@ -57,6 +84,10 @@ ipcMain.on(
             "output",
             folderName + "/output/",
         );
+        let ffm = "ffmpeg";
+        if(process.platform == "win32"){
+            ffm = "ffmpeg.exe";
+        }
         let comando = "";
         if(!cutvid){
             const theextension = usepng ? ".png" : ".jpg";
@@ -79,7 +110,7 @@ ipcMain.on(
                 fs.mkdirSync(outputDir, { recursive: true });
             }
             // comando = `ffmpeg -ss ${frome} -to ${toe} -i "${inputPath}" -q:v 2 "${outputPath}"`;
-            comando = `ffmpeg -ss ${frome} -to ${toe} -i "${inputPath}" -q:v 2 -fps_mode vfr -frame_pts true "${outputPath}"`;
+            comando = `${ffm} -ss ${frome} -to ${toe} -i "${inputPath}" -q:v 2 -fps_mode vfr -frame_pts true "${outputPath}"`;
 
         } else {
             outputPath = path.join(
@@ -88,7 +119,7 @@ ipcMain.on(
                 videoe + "_from_" + frome + "_until_" + toe +
                 ".mp4",
             );
-            comando = `ffmpeg -ss ${frome} -to ${toe} -i "${inputPath}" -c copy "${outputPath}"`;
+            comando = `${ffm} -ss ${frome} -to ${toe} -i "${inputPath}" -c copy "${outputPath}"`;
         }
         const ffmpeg = spawn(comando, { shell: true });
 
@@ -104,3 +135,28 @@ ipcMain.on(
         });
     },
 );
+
+ipcMain.on("fromyt", (event, link) => {
+    console.log("\n\n ...")
+    const namething = spawn((process.platform == "win32" ? "./" : "") + 'yt-dlp' + (process.platform == "win32" ? ".exe" : ""), ['--print', "%(title)s.%(ext)s", link]);   
+    let vname = ""
+    namething.stdout.on('data', (data) => {
+        console.log(data.toString());
+        vname = data.toString();
+
+    });
+    namething.on("close", (code) => {
+
+        if(code != 0) console.log(code);
+    });
+
+    console.log(link);
+
+    const ytdlp = spawn((process.platform == "win32" ? "./" : "") + "yt-dlp" + (process.platform == "win32" ? ".exe" : ""), [link, "-P", "./input/", "-o", "%(title)s.%(ext)s"]);
+    ytdlp.on("close", (code) => {
+        if(code != 0){
+            console.log(code);
+        }
+        event.reply("fromyt", vname)
+    })
+})  
--- renderer.js
+++ renderer.js
@@ -1,5 +1,7 @@
 const { ipcRenderer, ipcMain } = require('electron');
-
+const fs = require("fs");
+const path = require('path'); // or const path = require('path');
+const { disconnect } = require('process');
 
 document.getElementById('btn').addEventListener('click', () => {
     console.log(document.getElementById("cb").checked + " and " + document.getElementById("cb3").checked) 
@@ -15,9 +17,33 @@ document.getElementById('btn').addEventListener('click', () => {
 
 });
 document.getElementById("btnload").addEventListener("click", () =>{
-    ipcRenderer.send("makeFolder", document.getElementById("pau").value.replace(".mp4", ""));
-    document.getElementById("subject").src = "./input/" + document.getElementById("pau").value;
-    document.getElementById("outtext").innerText = "Output: ./" + document.getElementById("pau").value.replace(".mp4", "");
+    if(document.getElementById("fromyt").value != null && document.getElementById("fromyt").value !== ""){
+        ipcRenderer.send("fromyt", document.getElementById("fromyt").value)
+        document.getElementById("fromyt").value = "";
+    } else {
+        ipcRenderer.send("makeFolder", document.getElementById("pau").value.replace(".mp4", ""));
+        document.getElementById("subject").src = "./input/" + document.getElementById("pau").value;
+        document.getElementById("outtext").innerText = "Output: ./" + path.parse(document.getElementById("pau").value).name;        
+    }
+
+})
+ipcRenderer.on("fromyt", (event, msg) => {
+    console.log(msg);
+    console.log(msg);
+    ipcRenderer.send('getVideos');
+    document.getElementById("subject").src = "./input/" + msg;
+    document.getElementById("outtext").innerText = "Output: ./" + path.parse(msg).name; 
+    // let theop = 0;
+    // let theii = document.getElementById("pau").options.length - 1
+    // for(let i = theii; i >= 0; i--){
+    //     if(document.getElementById("pau").options[i].value == path.parse(msg).name){
+    //         // theop = i;
+    //         document.getElementById("pau").selectedIndex = i;
+    //         break;
+    //     } 
+    // }
+    // document.getElementById("pau").value = "AAAAA";
+
 
 })
 
@@ -25,8 +51,13 @@ ipcRenderer.send('getVideos');
 
 ipcRenderer.on("recieveVideos", (event, result) =>{
     let mer = "";
+    let bola = document.getElementById("pau")
+    // bola.querySelectorAll('select').forEach(eg => eg.remove())
+    var i, L = bola.options.length - 1;
+    for(i = L; i >= 0; i--) {
+        selectElement.remove(i);
+    }
     for(let i = 0; i < result.length; i++){
-        let bola = document.getElementById("pau")
         let butico = document.createElement("option")
         butico.text = result[i];
         bola.appendChild(butico)
@@ -38,3 +69,16 @@ ipcRenderer.on("extractframes-progress", (event, texx) => {
   document.getElementById("resultado").value += texx;
   document.getElementById("resultado").scrollTop = document.getElementById("resultado").scrollHeight;
 })
+// dialog.showMessageBox(window,{
+//     type: "warning",
+//     buttons: ["Ok"],
+//     defaultId: 0,
+//     cancelId: 0,
+//     title: "Install ffmpeg",
+//     message: "You dont have it",
+//     detail: "."
+// });
+
+// if(!fs.existsSync("ffmpeg.exe") && process.platform == "win32"){
+//     alert("Install ffmpeg.exe in the root folder");
+// }
